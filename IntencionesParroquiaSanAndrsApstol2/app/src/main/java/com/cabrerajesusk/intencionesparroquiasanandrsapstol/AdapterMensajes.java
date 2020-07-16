ebb45cc792c9c62684a564140c1afa7ab7a69746
Jesus <kechuu255@hotmail.com>
2020-07-16 00:26:46 -0300

    Esperando actualizacion

--- IntencionesParroquiaSanAndrsApstol2/app/src/main/java/com/cabrerajesusk/intencionesparroquiasanandrsapstol/AdapterMensajes.java
+++ IntencionesParroquiaSanAndrsApstol2/app/src/main/java/com/cabrerajesusk/intencionesparroquiasanandrsapstol/AdapterMensajes.java
@@ -4,9 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
